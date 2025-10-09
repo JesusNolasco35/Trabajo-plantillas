1f0b6fad8100f9fd5f509392a0406bd9ced12968
JesusNolasco35 <24308060610037@cetis61.edu.mx>
2025-10-08 19:56:08 -0600

    12

--- app.py
+++ app.py
@@ -4,25 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('plantilla-base.html')
-
-@app.route('/inicio')
-def inicio():
     return render_template('inicio.html')
 
-@app.route('/animales-exoticos')
+
+@app.route('/animales')
 def animales_exoticos():
     return render_template('animales-exoticos.html')
 
-@app.route('/vehiculos-antiguos')
+@app.route('/vehiculos')
 def vehiculos_antiguos():
-    return render_template('vehiculos')
+    return render_template('vehiculos.html')
 
-@app.route('/las-maravillas-del-mundo')
+@app.route('/maravillas')
 def las_maravillas_del_mundo():
     return render_template('las-maravillas-del-mundo.html')
 
-@app.route('/acerca-de')
+@app.route('/acerca')
 def acerca_de():
     return render_template('acerca-de.html')
 
