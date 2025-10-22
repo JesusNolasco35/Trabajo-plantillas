52d1c691a64b55f8ba57de860a0b8a03b201a137
JesusNolasco35 <24308060610037@cetis61.edu.mx>
2025-10-22 08:08:35 -0600

    121212

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+hola 
 
 app = Flask(__name__)
+app.secret_key = '2423415414'  
 
 @app.route('/')
-def home():
+def inicio():
+    return redirect(url_for('index')) 
+
+@app.route('/index')
+def index():
     return render_template('inicio.html')
 
+@app.route('/registro', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        nombres = request.form.get('nombres')
+        email = request.form.get('email')
+        contraseña = request.form.get('pass')  
+        confirmarcontraseña = request.form.get('passConfirm')  
+        dia = request.form.get('dia')
+
+
+        if contraseña != confirmarcontraseña:
+            flash("Las contraseñas no coinciden.", "error")
+            return redirect(url_for('registrar'))
+
+        print(f"Nuevo registro: {nombres}- {email}")
+        return redirect(url_for('index'))
+
+    return render_template('registro.html')
+
+@app.route('/acerca')
+def acerca():
+    return render_template('Acerca-de.html')     
 
 @app.route('/animales')
-def animales_exoticos():
+def animales():
     return render_template('animales-exoticos.html')
 
 @app.route('/vehiculos')
-def vehiculos_antiguos():
+def vehiculos():
     return render_template('vehiculos.html')
 
 @app.route('/maravillas')
-def las_maravillas_del_mundo():
-    return render_template('las-maravillas-del-mundo.html')
-
-@app.route('/acerca')
-def acerca_de():
-    return render_template('acerca-de.html')
+def maravillas():
+    return render_template('maravillas.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
